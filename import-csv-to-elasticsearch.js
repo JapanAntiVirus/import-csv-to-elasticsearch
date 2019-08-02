d93d67e5286dd35b6a4bc5fc1574ff06f2011f42
Tran Kim Tin <tintk62@wru.vn>
2019-08-02 23:59:50 +0700

    a

--- import-csv-to-elasticsearch.js
+++ import-csv-to-elasticsearch.js
@@ -33,7 +33,7 @@ client.ping({
         console.error('elasticsearch cluster is down!');
     } else {
         console.log('Everything is ok');
-        fs.createReadStream('../data2.csv')
+        fs.createReadStream('../data3.csv')
             .pipe(csv())
             .on('data', (row) => {
                 //   console.log(row);
@@ -42,10 +42,11 @@ client.ping({
                 delete r['_type'];
                 delete r['_index'];
                 delete r['_score'];
-                delete r['@timestamp'];
                 for (i in r) {
                     r[i] = r[i].replace(/,/g, '');
                 }
+                
+                //ots-*
                 r['scan_id'] = Number(r['scan_id']);
                 r['bandwidth_id'] = Number(r['bandwidth_id']);
                 r['traffic'] = Number(r['traffic']);
@@ -54,7 +55,7 @@ client.ping({
                 r['channel_status'] = Number(r['channel_status']);
                 r['port'] = Number(r['port']);
                 r['createdDate'] = new Date(r['createdDate'].replace('th', '').replace('rd','').replace('st',''));
-                
+                r['@timestamp'] = new Date(r['@timestamp'].replace('th', '').replace('rd','').replace('st',''));
                 if(r['trafficAvgOut'])
                     r['trafficAvgOut'] = Number(r['trafficAvgOut']);
                 else
@@ -63,24 +64,25 @@ client.ping({
                     r['trafficAvgIn'] = Number(r['trafficAvgIn']);
                 else
                     delete r['trafficAvgIn'];
-
-                if(r['bandwidth_id']!=0)
-                    insert(client,row,r);
-                // client.index({
-                //     index: row['_index'],
-                //     // id: row['_id'],
-                //     type: row['_type'],
-                //     body: r
-                // }, function (err, resp, status) {
-                //     // console.log(resp);
-                //     if(err){
-                //         console.log(err);
-                //     }
-                //     else{
-                //         console.log(resp);
-                //     }
-                // });
                 
+                //logstash-*
+                // r['createdDate'] = new Date(r['createdDate'].replace('th', '').replace('rd','').replace('st','').replace('nd',''));
+                // r['@timestamp'] = new Date(r['@timestamp'].replace('th', '').replace('rd','').replace('st','').replace('nd',''));
+                // r['sysUpTime_num'] = Number(r['sysUpTime_num']);
+                // r['cenAlarmUpdatedTimestamp_num'] = Number(r['cenAlarmUpdatedTimestamp_num']);
+                // r['sysUpTime_days'] = Number(r['sysUpTime_days']);
+                // r['cenAlertID'] = Number(r['cenAlertID']);
+                // r['cenAlarmVersion'] = Number(r['cenAlarmVersion']);
+                // r['cenAlarmUpdatedTimestamp_days'] = Number(r['cenAlarmUpdatedTimestamp_days']);
+                // r['cenAlarmType'] = Number(r['cenAlarmType']);
+                // r['cenAlarmTriageValue'] = Number(r['cenAlarmTriageValue']);
+                // r['cenAlarmTimestamp_num'] = Number(r['cenAlarmTimestamp_num']);
+                // r['cenAlarmTimestamp_days'] = Number(r['cenAlarmTimestamp_days']);
+                // r['cenAlarmStatus'] = Number(r['cenAlarmStatus']);
+                // r['cenAlarmSeverity'] = Number(r['cenAlarmSeverity']);
+                // r['cenAlarmCategory'] = Number(r['cenAlarmCategory']);
+
+                insert(client,row,r);
             })
             .on('end', () => {
                 console.log('CSV file successfully processed');
